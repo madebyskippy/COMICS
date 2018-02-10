e6336b7ab730895323ae775b9f03ff14c364e873
madebyskippy <timothys@nyu.edu>
2018-02-10 17:45:30 -0500

    polygon colliders but they dont move right

--- Assets/mvt_line.cs
+++ Assets/mvt_line.cs
@@ -6,6 +6,8 @@ public class mvt_line : MonoBehaviour {
 
 	//pretty loose coding since this is a prototype
 
+	int[] meshtopoly = new int[]{3,1,2,0};
+
 	GameObject dragging;
 	string dragPosition;
 
@@ -21,16 +23,14 @@ public class mvt_line : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			RaycastHit hit;
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {
-				//assuming the only colliders in the scene are the gutter dragging points
-				dragging = hit.transform.parent.gameObject;
-				dragPosition = hit.transform.name;
-				lastMouse = Input.mousePosition.x;
-			} else {
-				dragging = null;
-				dragPosition = "null";
+			RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction);
+			if (hit.transform != null){
+				if (hit.transform.tag == "gutter") {
+					dragging = hit.transform.gameObject;
+					dragPosition = hit.transform.name;
+					lastMouse = Input.mousePosition.x;
+				}
 			}
 		}
 		if (Input.GetMouseButtonUp (0)) {
@@ -53,15 +53,20 @@ public class mvt_line : MonoBehaviour {
 		}
 	}
 
+	//quad mesh points, from 
 	void nudgePoints(GameObject d, bool[] p, float c){	//dragged game object and veriticies being pulled and amount to change them
-		Mesh m = d.GetComponent<MeshFilter> ().mesh;
+		Mesh m = d.transform.parent.GetComponent<MeshFilter> ().mesh;
 		Vector3[] v = m.vertices;
+		PolygonCollider2D pc = d.GetComponent<PolygonCollider2D> ();
+		Vector2[] pcv = pc.points;
 		for (int i = 0; i < v.Length; i++) {
 			if (p [i]) {
 				//move it!!
 				v[i] = new Vector3(v[i].x+c, v[i].y,v[i].z);
+				pcv [meshtopoly [i]] = new Vector2 (pcv[meshtopoly[i]].x+c,pcv[meshtopoly[i]].y);
 			}
 		}
 		m.vertices = v;
+		pc.points = pcv;
 	}
 }
