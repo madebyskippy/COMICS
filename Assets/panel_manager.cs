7a71de943078197f0768b938dfeb8bec1f6c93da
madebyskippy <timothys@nyu.edu>
2018-02-13 22:11:15 -0500

    starting panel manager

---
+++ Assets/panel_manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_manager : MonoBehaviour {
+
+	[SerializeField] GameObject heart;
+	[SerializeField] GameObject tension;
+	[SerializeField] GameObject cornerPanel;
+
+	[SerializeField] mvt_line gutter1;
+	[SerializeField] mvt_line_alt gutter2;
+	[SerializeField] mvt_line_diag gutter3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
