cd7986a2b1fd6f1ed3b58af8ea605e01db1f1822
madebyskippy <timothys@nyu.edu>
2018-03-30 16:06:47 -0400

    setting up game, title screen, ch1 page switchign basics in

---
+++ Assets/game/Scripts/pageturner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pageturner : MonoBehaviour {
+
+	[SerializeField] GameObject[] pages;
+
+	// Use this for initialization
+	void Start () {
+		allDeactivate ();
+		pages [0].SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void allDeactivate(){
+		for (int i = 0; i < pages.Length; i++) {
+			pages [i].SetActive (false);
+		}
+	}
+
+	public void pageturn(int p){
+		allDeactivate ();
+		pages [p].SetActive (true);
+	}
+}
---
+++ Assets/game/Scripts/title_manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title_manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void switchScene(string s){
+		SceneManager.LoadScene ("ch1");
+	}
+}
