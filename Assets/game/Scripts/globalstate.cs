dcfa471792e5748076bd2805323eea946878969a
madebyskippy <timothys@nyu.edu>
2018-04-05 22:57:16 -0400

    mouse detects which tool to be and some canvas layering bs

---
+++ Assets/game/Scripts/globalstate.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalstate : MonoBehaviour {
+
+	private static globalstate instance = null;
+
+	//========================================================================
+
+	//code help from hang ruan :-)
+	public static globalstate Instance {
+		get { 
+			return instance;
+		}
+	}
+
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	//========================================================================
+
+	//string goes like this format: pg number - panel number - state number
+	Dictionary<string, bool> states = new Dictionary<string, bool>(){
+		{"pg1-p1-s1",false},
+		{"pg1-p1-s2",false},
+		{"pg1-p1-s3",false},
+	};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setState(string s, bool b){
+		if (states.ContainsKey (s)) {
+			states.Add (s, b);
+		}
+	}
+
+	public bool getState(string s){
+		bool b;
+		states.TryGetValue (s, out b);
+		return b;
+	}
+}
---
+++ Assets/game/Scripts/tool_manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tool_manager : MonoBehaviour {
+
+	string currentTool;
+	Image img;
+
+	// Use this for initialization
+	void Start () {
+		currentTool = "null";
+		img = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Input.mousePosition;
+		pos.z = 10f;
+		transform.position = Camera.main.ScreenToWorldPoint(pos);
+
+		if (currentTool == "gutter") {
+			img.color = Color.red;
+		} else if (currentTool == "sub") {
+			img.color = Color.green;
+		} else if (currentTool == "expanding") {
+			img.color = Color.blue;
+		} else if (currentTool == "null") {
+			img.color = Color.white;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log (col.tag);
+		currentTool = col.tag;
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		Debug.Log (col.tag);
+		currentTool = "null";
+	}
+}
