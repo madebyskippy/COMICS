7ebdc5dfd17b9981656091c63e41adf99937d5f1
Unknown <mkim2@andrew.cmu.edu>
2018-05-09 18:37:18 -0400

    sub-button animations and bunch of other icons

---
+++ Assets/game/Scripts/button_animations.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class button_animations : MonoBehaviour {
+
+    /*
+     * sub-button - animates while active
+     * pacer - click it and animates once
+     * gutter - animates while active
+     * */
+
+    Animator anim;
+    tool_manager toolManager;
+    bool enterOnce;
+	// Use this for initialization
+	void Start () {
+        enterOnce = false;
+        anim = GetComponent<Animator>();
+        toolManager = FindObjectOfType<tool_manager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch(toolManager.getTool()){
+            case "null":
+                enterOnce = false;
+                anim.SetBool("enteridle", true);
+                anim.SetBool("enteractive", false);
+                break;
+            case "sub-button":
+                SubButtonActive();
+                break;
+        }
+	}
+
+    void SubButtonActive(){
+        if (!enterOnce)
+        {
+            enterOnce = true;
+            anim.SetBool("enteridle", false);
+            anim.SetBool("enteractive", true);
+        }
+    }
+
+}
