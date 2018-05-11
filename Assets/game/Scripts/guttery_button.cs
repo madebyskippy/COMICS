5d3b7338b7f2f3e5208ea2c0e846a5591fbd9984
madebyskippy <timothys@nyu.edu>
2018-05-11 14:56:50 -0400

    can drag gutter from button now

--- Assets/game/Scripts/guttery_button.cs
+++ Assets/game/Scripts/guttery_button.cs
@@ -10,9 +10,12 @@ public class guttery_button : MonoBehaviour {
 
 	tool_manager tm;
 
+	BoxCollider2D col;
+
 	// Use this for initialization
 	void Start () {
 		tm = GameObject.FindObjectOfType<tool_manager> ();
+		col = GetComponent<BoxCollider2D> ();
 		triggerTargets (false);
 	}
 	
@@ -21,22 +24,28 @@ public class guttery_button : MonoBehaviour {
 		if (tm.getTool () != "gutter-button") {
 			gutter.setActive (false);
 			triggerTargets (false);
+//			col.enabled = true;
+			if (row=="2")
+				Debug.Log ("collider enabled cus tool not gutter-button "+tm.getTool());
 		} else {
 			if (tm.getActiveRow () != row) {
 				gutter.setActive (false);
 				triggerTargets (false);
+				//				col.enabled = true;
+				if (row == "2")
+					Debug.Log ("collider enabled cus active row not right");
+			} else {
+				gutter.setActive (true);
+				triggerTargets (true);
 			}
 		}
-	}
-
-	void OnMouseDown(){
-		gutter.setActive (true);
-		triggerTargets (true);
 	}
 
 	void triggerTargets(bool b){
 		for (int i = 0; i < targets.Length; i++) {
 			targets [i].SetActive (b);
 		}
+		col.enabled = !b;
 	}
+
 }
